c28e8ba5cd32eaafa73c65c45888ea2e11e6beeb
陈先生 <2246375441@qq.com>
2020-11-05 00:25:26 +0800

    搜索小窗口完成

--- src/components/searchboxan/SearchBoxAn.vue
+++ src/components/searchboxan/SearchBoxAn.vue
@@ -2,7 +2,7 @@
   <!-- w 100%  h 35px -->
   <div class="boxWk">
     <div class="box_but" @click="btnClick"><el-button type="primary" icon="el-icon-search" class="box">搜索</el-button></div>    
-    <div class="box_int"><el-input placeholder="请输入内容"></el-input></div>
+    <div class="box_int"><el-input v-model="inputValue" placeholder="请输入内容"  clearable @blur="inpBlur" class="fj" ref="input"></el-input></div>
   </div>
 </template>
 
@@ -12,17 +12,50 @@ export default {
   data() {
     return {
       // 按钮透明度
-      opacityData:1
+      opacityData:1,
+      // 搜索值
+      inputValue:''
     }
   },
   methods:{
     btnClick(){
       var btn = document.querySelector('.box_but')
-      setInterval(function(){
-        if (this.) {
-          
+      var int = document.querySelector('.box_int')
+
+      var that = this
+      var timeBtn = setInterval(function(){
+        if (that.opacityData > 0) {
+          that.opacityData =that.opacityData - 0.1
+
+          btn.style.opacity = that.opacityData
+          int.style.display= 'inline-block';
+          int.style.opacity = 1 - that.opacityData
+
+          // console.log(that.opacityData)
+        }else{
+          that.$refs.input.focus()
+          btn.style.display = 'none'
+          clearInterval(timeBtn)
         }
-      },100)
+      },50)
+    },
+    inpBlur(){
+      var btn = document.querySelector('.box_but')
+      var int = document.querySelector('.box_int')
+
+      var that = this
+      var timeBlur = setInterval(function(){
+        if (that.opacityData < 1) {
+          that.opacityData =that.opacityData + 0.1
+
+          btn.style.opacity = that.opacityData
+          btn.style.display= 'inline-block';
+          int.style.opacity = 1 - that.opacityData
+        }else{
+          int.style.display = 'none'
+          clearInterval(timeBlur)
+        }
+      },50)
     }
   }
 }
@@ -60,5 +93,8 @@ export default {
   top: 0!important;
   opacity: 0;
   display: none;
+}
+.fj{
+  box-shadow: 0 1px 3px rgba(50,50,93,.15), 0 1px 0 rgba(0,0,0,.02);
 }
 </style>
