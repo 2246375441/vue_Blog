b4ce62540f062be5ff917f0106c6582f7e344337
陈先生 <2246375441@qq.com>
2020-11-12 22:04:18 +0800

    完成部署设置框区域

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,20 @@
     <nav-bar></nav-bar>
     <background-map></background-map>
     <router-view></router-view>
+    <option-bar></option-bar>
   </div>
 </template>
 
 <script>
 import NavBar from './components/navbar/NavBar'
 import BackgroundMap from './components/backgroundmap/BackgroundMap'
+import OptionBar from './components/optionbar/OptionBar'
 export default {
   name:'App',
   components:{
     NavBar,
-    BackgroundMap
+    BackgroundMap,
+    OptionBar
   }
 }
 </script>
---
+++ src/components/optionbar/OptionBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="OptionBar">
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"OptionBar"
+}
+</script>
+
+<style scoped>
+.OptionBar{
+  position: fixed;
+  right: 0;
+  bottom: 0;
+
+  height: 150px;
+  width: 140px;
+  background-color: orange;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+}
+
+</style>
