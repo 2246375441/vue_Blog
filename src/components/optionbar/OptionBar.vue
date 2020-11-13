babc31112b164cb96e1d17e204d2d58f5dabd5bd
陈先生 <2246375441@qq.com>
2020-11-13 19:32:51 +0800

    全局设置按钮-左右布局

--- src/components/optionbar/OptionBar.vue
+++ src/components/optionbar/OptionBar.vue
@@ -1,14 +1,57 @@
 <template>
   <div class="OptionBar">
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
+    <op-position @LRclick="LRclick"></op-position>
   </div>
 </template>
 
 <script>
+import opPosition from './children/opPosition'
 export default {
-  name:"OptionBar"
+  name:"OptionBar",
+  data() {
+    return {
+      op_LR:'',
+      op_radius:'',
+    }
+  },
+  components:{
+    opPosition
+  },
+  created() {
+    this.DefaultSettings()
+  },
+  mounted(){
+    this.DefaultSettingsDom()
+  },
+  methods: {
+    // 初始化获取盒子 数据 比如左右 圆角
+    DefaultSettings(){
+      console.log(localStorage)
+      if(localStorage.getItem('op_LR')==null){localStorage.setItem('op_LR','left')}
+      if(localStorage.getItem('op_radius')==null){localStorage.setItem('op_radius','20px')}
+      
+
+      this.op_LR = localStorage.getItem('op_LR')
+      this.op_radius = localStorage.getItem('op_radius')
+
+    },
+    // 虚拟Dom处理
+    DefaultSettingsDom(){
+      var OptionBar = document.querySelector('.OptionBar')
+      this.op_LR = localStorage.getItem('op_LR')
+      if (this.op_LR==="left") {
+        OptionBar.style.left = 0
+        OptionBar.style.right = ""
+      }else{
+        OptionBar.style.left = ""
+        OptionBar.style.right = 0
+      }
+    },
+    LRclick(){
+      this.DefaultSettingsDom()
+      // console.log('发送')
+    }
+  },
 }
 </script>
 
@@ -20,7 +63,7 @@ export default {
 
   height: 150px;
   width: 140px;
-  background-color: orange;
+  /* background-color: orange; */
 
   display: flex;
   flex-direction: column;
---
+++ src/components/optionbar/children/opPosition.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="op_LR">
+    <!-- 左右切换按钮 -->
+    <el-tooltip
+      class="item"
+      effect="dark"
+      content="移至左侧"
+      placement="left"
+      id="op_LR_left"
+    >
+      <el-button
+        icon="el-icon-caret-left"
+        size="mini"
+        @click="leftClick"
+      ></el-button>
+    </el-tooltip>
+
+    <el-tooltip
+      class="item"
+      effect="dark"
+      content="移至右侧"
+      placement="left"
+      id="op_LR_right"
+    >
+      <el-button
+        icon="el-icon-caret-right"
+        size="mini"
+        @click="rightClick"
+      ></el-button>
+    </el-tooltip>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "opPosition",
+  mounted() {
+    console.log(localStorage.getItem("op_LR"));
+    if (localStorage.getItem("op_LR") === "left") {
+      document.querySelector("#op_LR_left").style.display = "none";
+      document.querySelector("#op_LR_right").style.display = "inline-block";
+      console.log("left");
+    } else {
+      document.querySelector("#op_LR_left").style.display = "inline-block";
+      document.querySelector("#op_LR_right").style.display = "none";
+      console.log("right");
+    }
+  },
+  methods: {
+    leftClick() {
+      // console.log("left");
+      localStorage.op_LR = "left";
+      this.$emit("LRclick", "发送修改事件触发");
+      document.querySelector("#op_LR_left").style.display = "none";
+      document.querySelector("#op_LR_right").style.display = "inline-block";
+    },
+    rightClick() {
+      // console.log("right");
+      localStorage.op_LR = "right";
+      this.$emit("LRclick", "发送修改事件触发");
+      document.querySelector("#op_LR_left").style.display = "inline-block";
+      document.querySelector("#op_LR_right").style.display = "none";
+    },
+  },
+};
+</script>
+
+<style scoped>
+#op_LR_left{
+  box-shadow: 0 15px 35px rgba(50,50,93,.1),0 5px 15px rgba(0,0,0,.07)!important;
+}
+#op_LR_left >>>.el-icon-caret-left{
+  color:#5e72e4;
+  font-size: 18px;
+}
+
+
+#op_LR_right{
+  box-shadow: 0 15px 35px rgba(50,50,93,.1),0 5px 15px rgba(0,0,0,.07)!important;
+}
+#op_LR_right >>>.el-icon-caret-right{
+  color:#5e72e4;
+  font-size: 18px;
+}
+</style>
