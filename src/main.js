70d3c743aee11603ff9abb22302cd86bddd2cb93
陈先生 <2246375441@qq.com>
2020-12-01 21:10:50 +0800

    😏添加jQuery1.9

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
 import Bus from './components/bus/bus'
+import $ from 'jquery' 
 
 import './assets/css/overallSituation.css' //导入全局默认css
 import './assets/css/root.css' //设置全局颜色--xx  var(--xx)
@@ -17,6 +18,7 @@ Vue.use(ElementUI);
 
 Vue.prototype.$bus = Bus  //将事件总线bus挂载到vue原型对象上
 Vue.prototype.$fz = fz    //将封装的JS挂载到vue原型对象上
+Vue.prototype.$ = $ //挂载jQuery
 
 new Vue({
   router,
